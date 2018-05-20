0c496475d8a62c778816185089e0ae53c28082e7
asabogal <asabogal.git@gmail.com>
2018-05-19 21:30:39 -0400

    environment config added

---
+++ config/environment.rb
@@ -0,0 +1,4 @@
+require_relative "../lib/top_credit_cards/version"
+require_relative "../lib/top_credit_cards/cli"
+require_relative "../lib/top_credit_cards/card"
+require_relative "../lib/top_credit_cards/scraper"
