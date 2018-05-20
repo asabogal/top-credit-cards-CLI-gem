3d1a01e2af1e28b426a1ce8880abeaf521b7c0c7
asabogal <asabogal.git@gmail.com>
2018-05-20 18:24:43 -0400

    initial card class functionality

--- lib/top_credit_cards/card.rb
+++ lib/top_credit_cards/card.rb
@@ -1,8 +1,23 @@
+require 'pry'
 class TopCreditCards::Card
 
-  attr_accessor :type, :name, :purchases_intro_apr, :balance_transfers_intro_apr
-  #etc... #add the rest of attributes from page
+  @@all = []
 
-  
+  attr_accessor :type, :name, :description, :purchases_intro_apr, :balance_transfers_intro_apr, :regular_apr, :annual_fee, :credit_needed, :apply_url
+
+  def initialze
+    @@all << self.class.all
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_from_page(page)
+    card = Card.new
+    #add card data attributes
+    card
+  end
+
 
 end
