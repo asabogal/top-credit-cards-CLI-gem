dffa57e1403063355a7261a452fffa69403fbb72
asabogal <asabogal.git@gmail.com>
2018-05-20 22:35:10 -0400

    developed initial test for scrper class

--- lib/top_credit_cards/scraper.rb
+++ lib/top_credit_cards/scraper.rb
@@ -1,7 +1,18 @@
-class TopCreditCards::Scraper
+# class TopCreditCards::Scraper
 
-  def call
-    
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+class Scraper
+  def self.scrape_test
+    doc = Nokogiri::HTML(open("https://www.creditcards.com/top-credit-cards.php"))
+    binding.pry
+    attributes = doc.css("li.first-row dl dt dd")
+    # attributes = doc.css("li.first-row dl dd").children.collect {|e| e}
+    ### need to develop this further
+    ### how will each card have it's own attribute value
+    ### create hash???
   end
-
+  self.scrape_test
 end
