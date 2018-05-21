9e904b7a2148fe4252ca8a331161e27a55bc40d0
asabogal <asabogal.git@gmail.com>
2018-05-20 20:13:38 -0400

    added corresponding instance variables

--- lib/top_credit_cards/card.rb
+++ lib/top_credit_cards/card.rb
@@ -3,9 +3,19 @@ class TopCreditCards::Card
 
   @@all = []
 
-  attr_accessor :type, :name, :description, :purchases_intro_apr, :balance_transfers_intro_apr, :regular_apr, :annual_fee, :credit_needed, :apply_url
+  attr_accessor :reward, :name, :description, :purchases_intro_apr, :balance_transfers_intro_apr, :regular_apr, :annual_fee, :credit_needed, :apply_url
 
   def initialze
+    @reward = reward
+    @name = name
+    @description = description
+    @purchases_intro_apr = purchases_intro_apr
+    @balance_transfers_intro_apr = balance_transfers_intro_apr
+    @regular_apr = regular_apr
+    @annual_fee = annual_fee
+    @credit_needed = credit_needed
+    @apply_url = apply_url
+
     @@all << self.class.all
   end
 
