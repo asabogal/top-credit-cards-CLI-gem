411da3e05812dbc404c96fb8efb9c72ab79f1e4c
asabogal <asabogal.git@gmail.com>
2018-05-21 18:15:22 -0400

    added partial functionality; need to improve card instantiation

--- lib/top_credit_cards/scraper.rb
+++ lib/top_credit_cards/scraper.rb
@@ -6,48 +6,48 @@ require 'pry'
 
 require_relative './card.rb'
 
-  #scraper draft
-    # @@attributes = {}
+  #scraper draft // need to test!!
+  class Scraper
+    @@attributes = []
 
-    def self.scrape_data
+    def self.scrape_card_data
       doc = Nokogiri::HTML(open("https://www.creditcards.com/top-credit-cards.php"))
-        attributes = doc.css("div.res-schumer-box")
-          attributes.each do |card|
-            name = card.first.css("div.res-offer-left a").text.strip
-
+        data = doc.css("div.res-schumer-box")
+          data.each do |card|
+            name = card.css("div.res-offer-left a").text.strip
+            description = card.css("div.res-details li").text.strip
+            purchases_intro_apr = card.css("li.first-row dd")[0].text
+            balance_transfers_intro_apr = card.css("li.first-row dd")[1].text
+            regular_apr = card.css("li.first-row dd")[2].text
+            annual_fee = card.css("li.first-row dd")[3].text
+            credit_needed = card.css("li.first-row dd")[4].text
+            apply_url = card.css("div.res-field-apply-now a").attribute("href").value
+            @@attributes << {name: name, description: description, purchases_intro_apr: purchases_intro_apr, balance_transfers_intro_apr: balance_transfers_intro_apr, regular_apr: regular_apr, annual_fee: annual_fee, credit_needed: credit_needed, apply_url: apply_url}
           end
 
-        financial_data = first.css("dd").children
-          @@attributes << {:name, :description, :purchases_intro_apr, :balance_transfers_intro_apr, :regular_apr, :annual_fee, :credit_needed, :apply_url}
     end
+
 
     def self.scrape_reward
+      doc = Nokogiri::HTML(open("https://www.creditcards.com/top-credit-cards.php"))
+        doc.css("div.row").each do |name|
+        reward = name.css("h2").text
+          @@attributes << {reward: reward}
+      end
 
-      doc.css("div.row").each do |reward|
-        reward =
     end
 
-###____###
-# card container = doc.css("div.res-schumer-box")
-# card.name = doc.css("div.res-schumer-box").first.css("div.res-offer-left a").text.strip
-#card.description = doc.css("div.res-schumer-box").first.css("div.res-details li").text.strip
-#apr_details_container = doc.css("div.res-schumer-box").first.css("dd").children
-  ##iterate each to create apr_attributes i.e :purchases_intro_apr, :balance_transfers_intro_apr, :regular_apr, :annual_fee, :credit_needed
-
-class Scraper
-
-  def self.scrape_name_description_apply_rul
-    doc = Nokogiri::HTML(open("https://www.creditcards.com/top-credit-cards.php"))
-    # binding.pry
-    # doc.css("div.res-schumer-box").each do |e|
+## not ideal but use for now
+    def self.make_cards
+      15.times do
       card = Card.new
-      card.name = doc.css("div.res-schumer-box").first.css("div.res-offer-left a").text.strip
-      card.description = doc.css("div.res-schumer-box").first.css("div.res-details li").text.strip
+      scrape_reward
+      scrape_card_data
       card.save
-    # end
-    binding.pry
-  end
+      end
+      binding.pry
+    end
+Scraper.make_cards
 
 
 end
-Scraper.get_page
